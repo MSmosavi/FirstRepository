dde01cdf74571cefe99d591aefb87f6a42ab11a7
Shams <Shams@C13646>
2021-02-23 23:37:50 +0330

    add Stop Botton

--- TestFirst/Form1.Designer.cs
+++ TestFirst/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace TestFirst
         private void InitializeComponent()
         {
             this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnStart
@@ -42,11 +43,21 @@ namespace TestFirst
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(29, 149);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(454, 39);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(511, 280);
+            this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +68,7 @@ namespace TestFirst
         #endregion
 
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
     }
 }
 
